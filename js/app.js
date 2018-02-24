3f7360fa4782e16c08fd52ae548eb767e0a9bf1b
SnowLCash <Yuuomi@Christophers-MacBook-Pro.local>
2018-02-23 19:31:16 -1000

    comp Harmony

--- js/app.js
+++ js/app.js
@@ -1,19 +1,36 @@
 /*Hey Prepsters! Let's help Ronald McDonald find true love by revising his online dating profile!*/
 
 // 1. In the nav element, target the span element with the id of "count" and change the number from 0 to 8.
-
+var countElement = document.getElementById("count");
+countElement.innerHTML = 8;
 
 
 // 2. Target the div with the id of 'age' and change the contents to: "21 year old vibrant stud".
-
+var ageE = document.getElementById("age").innerHTML = '21 year old vibrant stud';
 
 
 // 3. Target the span element with the id of "distance" and change the number from 1000 to 168.
+var disElem = document.getElementById('distance').innerHTML = 168;
 
 
 // 4. Create a paragraph element with the id of "pro2". In this paragraph, add the following content: "My main objective is to find a Mrs. McDonald." Append this element inside of the paragraph element with the id of "profile".
-
-
+var pro2Elem = document.getElementById('p');
+pro2Elem.id = "pro2";
+pro2Elem.innerHTML = 'My main objective is to find Mrs. McDonald.';
+profile.appendChild(pro2Elem);
 // 5. Create a span element of the id of "mail" and give it a content of 11. Append this element into the "Messages" li element.
+var mailId = document.getElementById('span');
+mailId.id = 'mail';
+mailId.innnerHTML = 11;
+Messages.appendChild(mailId);
+
+/* Examples of Jace's code: 
+var mango = document.createElement("span")
+mango.id = 'mail';
+mango.innerHTML = 11;
+var peach = document.getElementsBuClassName("menu")
+consoel.log(peache)
+peach[2}.appendChild(mango);*/
+
 
 
